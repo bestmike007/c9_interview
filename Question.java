89f999d9d72745a3ce23f61be304e3458ba24117
mc_interview <mc_interview@163.com>
2015-05-19 07:25:39 +0000

    Add comments.

--- Question.java
+++ Question.java
@@ -4,6 +4,8 @@ import java.io.*;
 public class Question {
 
     public static void main(String[] args) throws Exception {
+        // You can run this file by `Menu -> Run -> Run Configurations -> Test`
+        // Try to write your hello world program.
         System.out.println("Cong!");
     }
 
